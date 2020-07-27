d7ebf61189649ca7dfcb2b1c8b82ceed3ff451a5
dajamante <aimaiga2@gmail.com>
2020-07-27 07:03:04 +0200

    mutex stage

--- pipeviewer/src/main.rs
+++ pipeviewer/src/main.rs
@@ -1,25 +1,44 @@
-use std::env;
-use std::io::{self, Read, Write};
+use pipeviewer::{args::Args, read, stats, write};
+//accessing functions through their modules
+use std::io::Result;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-const CHUNK_SIZE: usize = 16 * 1024;
 //cargo fmt
 
-fn main() {
-    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let Args {
+        infile,
+        outfile,
+        silent,
+    } = args;
+
+    //mutex guarding boolean
+    let quit = Arc::new(Mutex::new(false));
+
+    // three clones of quit. To coordinate clean up and closing.
+    let (quit1, quit2, quit3) = (quit.clone(), quit.clone(), quit.clone());
     // command D copies last line
-    let mut total_bytes = 0;
-    loop {
-        let mut buffer = [0; CHUNK_SIZE];
-        let num_read = match io::stdin().read(&mut buffer) {
-            Ok(0) => break,
-            Ok(x) => x,
-            Err(_) => break,
-        };
-        total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
-    }
-    if !silent {
-        eprintln!("num_read: {}", total_bytes);
-    }
+
+    let read_handle = thread::spawn(move || read::read_loop(&infile, quit1));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, quit2));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, quit3));
+
+    //join threads and crash if any thread has crashed
+    //`.join()`returns a thread::Result::<io::Result<()>>
+
+    let read_io_result = read_handle.join().unwrap();
+    let stats_io_result = stats_handle.join().unwrap();
+    let write_io_result = write_handle.join().unwrap();
+
+    //return error if any of them are errors
+
+    read_io_result?;
+    stats_io_result?;
+    write_io_result?;
+
+    Ok(())
 }
+
 //ctrl D is the sign for end of file.
